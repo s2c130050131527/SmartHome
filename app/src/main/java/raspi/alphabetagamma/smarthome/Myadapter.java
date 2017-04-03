8f8901cfbd12907fafb6ef919752244208e093de
Shrey Naygandhi <shreynaygandhi@live.com>
2017-04-03 13:55:40 +0530

    removed comments

--- app/src/main/java/raspi/alphabetagamma/smarthome/Myadapter.java
+++ app/src/main/java/raspi/alphabetagamma/smarthome/Myadapter.java
@@ -23,21 +23,6 @@ public class Myadapter extends SimpleAdapter {
     private Context ctx;
     private String ipAddress;
 
-
-
-    /**
-     * Constructor
-     *  @param context  The context where the View associated with this SimpleAdapter is running
-     * @param data     A List of Maps. Each entry in the List corresponds to one row in the list. The
-     *                 Maps contain the data for each row, and should include all the entries specified in
-     *                 "from"
-     * @param resource Resource identifier of a view layout that defines the views for this list
- *                 item. The layout file should include at least those named views defined in "to"
-     * @param from     A list of column names that will be added to the Map associated with each
-*                 item.
-     * @param to       The views that should display column in the "from" parameter. These should all be
-     * @param ipAddress
-     */
     public Myadapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to, String ipAddress) {
         super(context, data, resource, from, to);
         ctx=context;
